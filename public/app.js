048116d41301cf1fe7c4278f2925acea1ecd0516
vhmvictor <victor.hugomarques@hotmail.com>
2020-09-14 12:40:58 -0300

    atualizacao add-to-home-screen

--- public/app.js
+++ public/app.js
@@ -1,18 +1,22 @@
-var e = " Propriedade userAgent: " +navigator.userAgent;
-document.getElementById("info").innerHTML = e;
-
 var navegadores = "";
-
+let browser = "";
 if (navegadores = navigator.userAgent.toLowerCase().indexOf('op') > -1) {
-    document.getElementById("browser").innerHTML = "Opera";
+    browser = "Opera";
 }else if (navegadores = navigator.userAgent.indexOf('Firefox') > -1) {
-    document.getElementById("browser").innerHTML = "Mozilla Firefox";
+    browser = "Mozilla Firefox";
 }else if (navegadores = navigator.userAgent.indexOf('Epiphany') > -1) {
-    document.getElementById("browser").innerHTML = "Epiphany";
+    browser = "Epiphany";
 }else if (navegadores = navigator.userAgent.indexOf('Edg') > -1) {
-    document.getElementById("browser").innerHTML = "Microsoft Edge"
+    browser = "Microsoft Edge";
 }else if (navegadores = navigator.userAgent.indexOf('Chrome') > -1) {
-    document.getElementById("browser").innerHTML = "Google Chrome";
+    browser = "Google Chrome";
 }else if (navegadores = navigator.userAgent.indexOf('Safari') > -1) {
-    document.getElementById("browser").innerHTML = "Safari";
+    browser = "Safari";
 }
+console.log(browser)
+if (browser == "Safari" || browser == "Opera") {
+    Swal.fire({
+        imageUrl: "add-home.gif",
+        confirmButtonText: "Entendi!", 
+    });
+};
